ad74118321956113970af968fc41c7f5ae69c794
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-12-01 12:56:53 +0530

    Project Structure

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("SITEURL", "/contactbook/");
-
-function print_arr($arr)
-{
-  echo "<pre>";
-  print_r($arr);
-}
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Discussion Board</title>
+  <?php
+  include_once("./client/commonFiles.php");
+  ?>
+</head>
+<body>
+  <?php
+  include_once("./client/header.php");
+  ?>
+</body>
+</html>
